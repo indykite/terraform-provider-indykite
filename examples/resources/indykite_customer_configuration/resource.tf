612bb5320e86ebe51316ff54fede79261ad042d5
Pavel Kutáč <pavel.kutac@gmail.com>
2024-07-15 15:23:22 +0200

    docs: fix documentation templates

--- examples/resources/indykite_customer_configuration/resource.tf
+++
@@ -1,6 +0,0 @@
-resource "indykite_customer_configuration" "development" {
-  customer_id = "gid:customer-gid"
-
-  default_auth_flow_id     = "gid:auth-flow-gid"
-  default_email_service_id = "gid:email-gid"
-}
