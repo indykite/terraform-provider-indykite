6fc98ea49bf05d287d7976d5b6448b9790bf7b24
Cowan Macady <cowan.macady@gmail.com>
2025-05-09 09:41:49 +0200

    chore: upgrade go version and dependencies
    implement ENG-6412

--- examples/data-sources/indykite_application_agent/data-source.tf
+++ examples/data-sources/indykite_application_agent/data-source.tf
@@ -1,5 +1,5 @@
 data "indykite_application_agent" "application_agent" {
   app_space_id = "ApplicationSpaceGID"
   app_agent_id = "ApplicationAgentGID"
-  name = "AppAgentName"
+  name         = "AppAgentName"
 }
