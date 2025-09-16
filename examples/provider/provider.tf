6b7be3b2afddb41929f1bc993e73d3c243fd9b0a
Alex P <130386437+alexpriv8@users.noreply.github.com>
2025-09-15 21:36:51 -0600

    ci: tfplugindocs fix

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,15 @@
 # indykite provider integrates IndyKite platform with Terraform scripting.
+
+terraform {
+  required_version = ">= 1.9"
+
+  required_providers {
+    indykite = {
+      source  = "indykite/indykite"
+      version = ">=0.0.1"
+    }
+  }
+}
+
 # Provider for now does not support any parameters and all is set within service account credential file.
 provider "indykite" {}
